f5545ecf19739a56510cacb69cab776c62729717
James Clark <jamesclark@James-MacBook-Pro.local>
2019-10-26 22:09:51 -0700

    after first video

---
+++ src/material-design/material-design.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatCheckboxModule,
+    MatButtonModule,
+   MatCardModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  exports: [
+    MatCheckboxModule,
+    MatButtonModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatIconModule,
+    MatToolbarModule
+  ]
+})
+export class MaterialDesignModule { }
